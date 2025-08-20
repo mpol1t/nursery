cf7e1912631b1f8f5f23264107e5f8b4b7d6aa1e
Michal Polit <mpolit@protonmail.com>
2025-08-20 12:08:39 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nursery.MixProject do
   def project do
     [
       app:               :nursery,
-      version:           "0.1.1",
+      version:           "0.1.2",
       elixir:            "~> 1.16",
       start_permanent:   Mix.env() == :prod,
       deps:              deps(),
